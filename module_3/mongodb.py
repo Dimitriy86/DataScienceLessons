7934da916c96ecb47ddd40e27ca05cfc7e056ccd
dmitr <dmitriyshirin86@gmail.com>
2025-12-08 20:02:59 +0100

    Connect to DB

---
+++ module_3/mongodb.py
@@ -0,0 +1,34 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+url = "mongodb+srv://dmitriyshirin86_db_user:AIJCIC3mOu3YDipH@cluster0.mfvqt3z.mongodb.net/?appName=Cluster0"
+client = MongoClient(url, server_api=ServerApi('1')
+)
+
+db = client.book
+
+result_one = db.cats.insert_one(
+    {
+        "name": "barsik",
+        "age": 3,
+        "features": ["ходить в капці", "дає себе гладити", "рудий"],
+    }
+)
+
+print(result_one.inserted_id)
+
+result_many = db.cats.insert_many(
+    [
+        {
+            "name": "Lama",
+            "age": 2,
+            "features": ["ходить в лоток", "не дає себе гладити", "сірий"],
+        },
+        {
+            "name": "Liza",
+            "age": 4,
+            "features": ["ходить в лоток", "дає себе гладити", "білий"],
+        },
+    ]
+)
+print(result_many.inserted_ids)
